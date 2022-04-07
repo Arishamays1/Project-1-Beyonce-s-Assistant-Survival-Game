29297c8eb92659cbe977b6da3d19cae90ed122e6
arishamays1 <1arishamays1@gmail.com>
2022-04-07 11:51:59 -0400

    fixed issue. Began option cascade.

--- app.js
+++ app.js
@@ -93,22 +93,88 @@ patienceStat.innerHTML=beyonce.patience
 // function for the options
 function selectOption(option){
     console.log("this should be checked for hunger --")
-    console.log("this is button.innertext", option)
-    if(option===textNodes[0].options[0].text){
-       beyonce.hunger -=2,
+    console.log("this is button.innertext", option.text)
+    const nextTextNodeId = option.nextText
+    showTextNode(nextTextNodeId)
+    //level 1
+    if(option.text===textNodes[0].options[0].text){
+       beyonce.hunger -=1,
        hungerStat.innerHTML= beyonce.hunger
        console.log('it worked')
        console.log(beyonce.hunger)
+       beyonce.happiness += 1
+       happyStat.innerHTML=beyonce.happiness
+       beyonce.patience += 1
+       patienceStat.innerHTML=beyonce.patience
     }
-    if(option===textNodes[0].options[1].text){
-        beyonce.hunger -=2,
+    if(option.text===textNodes[0].options[1].text && gameActive===true){
+        beyonce.hunger -=3,
         hungerStat.innerHTML= beyonce.hunger
-       beyonce.happiness += 1,
+        beyonce.happiness += 3
+        happyStat.innerHTML=beyonce.happiness
+        beyonce.patience += 2
+        patienceStat.innerHTML=beyonce.patience
         console.log('it worked')
         console.log(beyonce.hunger)
+        //I need to create the function to not allow any other button to be clicked unless 
 }
+    if(option.text===textNodes[0].options[2].text && gameActive===true){
+        console.log('it worked')
+        console.log(beyonce)
+        stopGame()
+    }
+    //end of Level 1
+//I need to create the function to not allow any other button to be clicked unless
+    //Level 2 if granola is picked
+        //calling blue
+    if(option.text===textNodes[1].options[0].text && gameActive===true){
+            beyonce.happiness += 1
+            happyStat.innerHTML=beyonce.happiness
+            beyonce.patience += 1
+            patienceStat.innerHTML=beyonce.patience
+            console.log('it worked')
+            console.log(textNodes[1].options[0].text)
+            
+             
+    }
+    //calling rumi
+    if(option.text===textNodes[1].options[1].text && gameActive===true){
+        console.log('it worked')
+        console.log(beyonce)
+        beyonce.patience = 0
+        patienceStat.innerHTML=beyonce.patience
+        stopGame()
+
 }
-//}
+//Level 2 if granola is picked
+        //calling blue
+        if(option.text===textNodes[2].options[0].text && gameActive===true){
+            beyonce.happiness += 1
+            happyStat.innerHTML=beyonce.happiness
+            beyonce.patience += 1
+            patienceStat.innerHTML=beyonce.patience
+            console.log('it worked')
+            console.log(textNodes[1].options[0].text)
+            
+             
+    }
+    //calling rumi
+    if(option.text===textNodes[2].options[1].text && gameActive===true){
+        console.log('it worked')
+        console.log(beyonce)
+        beyonce.patience = 0
+        patienceStat.innerHTML=beyonce.patience
+        stopGame()
+
+}
+
+
+}
+
+function stopGame(){
+    gameActive= false
+    btnElement.classList.add('rm')
+}
 
 
 /*function hunger(2,1, 1){
@@ -132,6 +198,7 @@ function selectOption(option){
 // pop up when the play button is clicked
 const textNodes = [
     {
+         //this is textnodes[0]
         id: 1,
         text: "You're starting your first day off at her house, make sure to assist all her needs and demands, she's going to an event today what are you having her prepared for breakfast?",
         options: [
@@ -156,6 +223,7 @@ const textNodes = [
             },
         ]
     },
+     //this is textnodes[1]
     {
         id: 2.0,
         text: "She nods at you for choosing the healthy breakfast, then ask you to FaceTime her daughter who are you FaceTiming, Blue Or Rumi?",
@@ -172,6 +240,7 @@ const textNodes = [
             }
         ]
     },
+     //this is textnodes[2]
     {
         id: 2.1,
         text: "She smiles and says, 'How did you know that was my favorite girl??' She then asks you toFaceTime her daughter who are you FaceTiming, Blue Or Rumi?",
@@ -186,9 +255,50 @@ const textNodes = [
                 nextText: 3.1
             }
         ]
-    }
+    },
+     //this is textnodes[3]
+    {
+        id: 3.0,
+        text: "Beyonce Facetimes Blue Ivy. Bey now needs something to do while waiting for her hair and makeup team. What do you suggest?",
+        options: [
+            {
+                text: "Swimming",
+                nextText: 4.0
+            },
+            {
+                text: "Painting",
+                nextText: 4.1
+
+            },
+            {
+                text: "Gossiping and Drinking",
+                nextText: 4.2
+            }
+        ]
+    },
+     //this is textnodes[4]
+    {
+        id: 3.1,
+        text: "Beyoncé is silent and gives you a dirty look... She then says 'she's only two', then has her team of lawyers send you a termination letter. You're fired"
+    },
+
+    //this is textnodes[5]
+    {  
+        id: 4.0,
+        text: "Beyoncé goes and gets ready to go swimming, Beyoncé has three different types of pools, which pool should Beyoncé swim in?",
+        options: [
+            {
+                text:"Chlorine Pool",
+                nextText: 5.0
+            },
+            {
+                text: "Mineral Pool",
+                nextText: 5.1
+            }
+        ]
+    },
 ]
 
 
-console.log(option===textNodes[0].options[0].text)
+//console.log(option===textNodes[0].options[0].text)
 
